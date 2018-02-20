96f50a70edd39435dcc67d978223da8213ed3122
Benjamin <sabean.ben@gmail.com>
2018-02-20 07:55:28 -0400

    Added multithreading

--- server.c
+++ server.c
@@ -6,12 +6,44 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <pthread.h>
 
-#define BUFFSIZE 255
+#define BUFFSIZE 256
 
 void error(const char *msg) {
     perror(msg);
     exit(1);
+}
+
+// this function is run by the second thread 
+void *Trajectory(int *newsockfd) {
+    int n = 0;
+    char inbuffer[BUFFSIZE];
+    char outbuffer[BUFFSIZE];
+
+    bzero(inbuffer, BUFFSIZE);
+    bzero(outbuffer, BUFFSIZE);
+    sprintf(outbuffer, "World\n"); 
+
+    printf("Client Successfully connected,\n");
+
+
+    n = read(*newsockfd,inbuffer,BUFFSIZE-1);   // get the string sent from the client
+    if (n < 0)
+        error("ERROR reading from socket");
+    printf("Received: [%s]\n",inbuffer);         // and dump it to string
+
+    n = write(*newsockfd,outbuffer,strlen(outbuffer+1)); // then send it back
+    if (n < 0)
+        error("ERROR writing to socket");
+    printf("Sent    : [%s]\n",outbuffer);
+
+printf("Exiting Thread\n");
+//close(*newsockfd); 
+
+// the function must return something
+return NULL;
+
 }
 
 int main(int argc, char *argv[]) {
@@ -21,16 +53,11 @@ int main(int argc, char *argv[]) {
     int newsockfd = 0;
     socklen_t clilen;
     struct sockaddr_in cli_addr;
-    char inbuffer[BUFFSIZE];
-    char outbuffer[BUFFSIZE];
-    int n = 0;
  
-    bzero(inbuffer, BUFFSIZE);
-    bzero(outbuffer, BUFFSIZE);
-    sprintf(outbuffer, "World\n"); 
-
-    if (argc < 2)
+    if (argc < 2) {
         error("ERROR, no port provided\n");
+        return EXIT_FAILURE;
+    }
 
     memset(&cli_hints, 0, sizeof cli_hints);
     cli_hints.ai_family = AF_UNSPEC;
@@ -52,27 +79,27 @@ int main(int argc, char *argv[]) {
         error("ERROR on binding");
         return EXIT_FAILURE;  
     }    
+    while(1) {
+        listen(sockfd,5);     // block and wait for a new connection from a client
+        clilen = sizeof(cli_addr);     // get address of client
+        newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+        printf("New client connecting.\n");
 
-    listen(sockfd,5);     // block and wait for a new connection from a client
-    clilen = sizeof(cli_addr);     // get address of client
-    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+        if (newsockfd < 0) {
+            error("ERROR on accept");
+            return EXIT_FAILURE;
+        }
 
-    if (newsockfd < 0) {
-        error("ERROR on accept");
-        return EXIT_FAILURE;
+        // this variable is our reference to the second thread 
+        pthread_t inc_x_thread;
+
+        // create a second thread
+        if(pthread_create(&inc_x_thread, NULL, (void *)Trajectory, &newsockfd)) {
+            error("Error creating thread");
+            return EXIT_FAILURE;
+        }
     }
-
-    n = read(newsockfd,inbuffer,BUFFSIZE-1);   // get the string sent from the client
-    if (n < 0)
-        error("ERROR reading from socket");
-    printf("Received: [%s]\n",inbuffer);         // and dump it to string
-
-    n = write(newsockfd,outbuffer,strlen(outbuffer+1)); // then send it back
-    if (n < 0)
-        error("ERROR writing to socket");
-    printf("Sent    : [%s]\n",outbuffer);
-
-    close(newsockfd);   
+  
     close(sockfd);    
     return EXIT_SUCCESS;  
 }
